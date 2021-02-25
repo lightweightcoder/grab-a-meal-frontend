5e7f0c966f449675345f115c2bb73b6195a21df9
Kenrickles <kenrickles@gmail.com>
2021-02-26 00:44:52 +0800

    Fixed the merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import NavbarComponent from './components/NavBar.jsx';
 import LoginComponent from './components/Login.jsx';
 import RegistrationComponent from './components/Registration.jsx';
 import HomeComponent from './components/Home.jsx';
+import CreateActivityComponent from './components/CreateActivity.jsx';
 import { AppProvider } from './store.jsx';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
             <Route path="/profile" component="" />
             <Route path="/messages" component="" />
             <Route path="/logout" component="" />
+            <Route path="/activities/new" component={CreateActivityComponent} />
             <Route path="/login" component={LoginComponent} />
             <Route path="/register" component={RegistrationComponent} />
           </Switch>
