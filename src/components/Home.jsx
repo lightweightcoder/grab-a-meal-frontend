37fdd9610117080bfd62048312747cb1af34a5cd
aljt <alvinljt@gmail.com>
2021-03-01 23:40:12 +0800

    added functionality to display user action buttons depending on user's relation to the activity

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 import { Modal, Button, ButtonGroup } from 'react-bootstrap';
 import moment from 'moment';
 import CardComponent from './Card.jsx';
@@ -11,9 +12,12 @@ import {
 } from '../store.jsx';
 
 export default function HomeComponent() {
+  // set the current cookies (stored in the browser) in the cookies state
+  const [cookies] = useCookies([]);
   const { store, dispatch } = useContext(AppContext);
   const { activities } = store;
   const [displayCardDetails, setdisplayCardDetails] = useState(false);
+  const [editOrViewActivity, setEditOrViewActivity] = useState('VIEW');
   const [activityDetails, setActivityDetails] = useState({
     id: '',
     name: '',
@@ -23,6 +27,7 @@ export default function HomeComponent() {
     location: '',
     creatorId: '',
     creatorName: '',
+    participants: [],
   });
 
   // create a hook to use when the logic says to change components
@@ -70,40 +75,106 @@ export default function HomeComponent() {
     });
   };
 
-  const cardSelectionModal = () => (
-    <Modal show={displayCardDetails} onHide={handleDisplayClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>{activityDetails.name}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        Description:
-        {' '}
-        {activityDetails.description}
-        <br />
-        Location:
-        {' '}
-        {activityDetails.location}
-        <br />
-        Date:
-        {' '}
-        {moment(activityDetails.dateTime).format('ll')}
-        <br />
-        Participants:
-        {' '}
-        {`${activityDetails.participants.length}/${activityDetails.totalNumOfParticipants}`}
-        <br />
-        Event Organized By:
-        {' '}
-        {activityDetails.creatorName}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="success" onClick={handleJoinActivity(activityDetails.id)}> Join </Button>
-        <Button variant="secondary" onClick={handleDisplayClose}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+  const cardSelectionModal = () => {
+    // get the userId from the browser cookie
+    const userId = Number(cookies.userId);
+    console.log('userId is', userId);
+
+    // if state is view, show activity details,
+    // and show buttons based on the user's relation to the activity
+    // if state is edit, show form to edit activity details
+
+    if (editOrViewActivity === 'VIEW') {
+      let userActionsButtons = '';
+      // get the buttons based on the user's relation to the activity:
+      // 1. user is the creator
+      if (userId === activityDetails.creatorId) {
+        userActionsButtons = (
+          <>
+            <Button variant="primary">Chat</Button>
+            <Button variant="success">Edit</Button>
+            <Button variant="danger">Delete</Button>
+          </>
+        );
+      } else if (activityDetails.participants.find((el) => el.id === userId)) {
+        // 2. user is a current participant and not the creator
+        userActionsButtons = (
+          <>
+            <Button variant="primary">Chat</Button>
+            <Button variant="danger">Leave</Button>
+          </>
+        );
+      } else {
+        // 3. user is a potential participant
+        userActionsButtons = (
+          <>
+            <Button variant="success" onClick={handleJoinActivity(activityDetails.id)}> Join </Button>
+          </>
+        );
+      }
+
+      // return the modal
+      return (
+        <Modal show={displayCardDetails} onHide={handleDisplayClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{activityDetails.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Description:
+            {' '}
+            {activityDetails.description}
+            <br />
+            Location:
+            {' '}
+            {activityDetails.location}
+            <br />
+            Date:
+            {' '}
+            {moment(activityDetails.dateTime).format('ll')}
+            <br />
+            Participants:
+            {' '}
+            {`${activityDetails.participants.length}/${activityDetails.totalNumOfParticipants}`}
+            <br />
+            Event Organized By:
+            {' '}
+            {activityDetails.creatorName}
+          </Modal.Body>
+          <Modal.Footer>
+            {userActionsButtons}
+            <Button variant="secondary" onClick={handleDisplayClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      );
+    }
+
+    if (editOrViewActivity === 'EDIT') {
+      // return the form to edit activity details
+      return (
+        <Modal show={displayCardDetails} onHide={handleDisplayClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{activityDetails.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Event Organized By:
+            {' '}
+            {activityDetails.creatorName}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success" onClick={handleJoinActivity(activityDetails.id)}> Save Changes </Button>
+            <Button variant="secondary" onClick={handleDisplayClose}>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      );
+    }
+
+    // return an empty div so eslint will not throw error
+    return <div />;
+  };
 
   const activityDisplay = () => {
     const activityFeed = activities.map((activity) => (
