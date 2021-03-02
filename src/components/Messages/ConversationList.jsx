6cd2a4fb2e32d74d9cd72655373c0f183bbe94d3
Kenrickles <kenrickles@gmail.com>
2021-03-02 22:27:16 +0800

    fixed array bugs

--- src/components/Messages/ConversationList.jsx
+++ src/components/Messages/ConversationList.jsx
@@ -1,38 +1,115 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import ConversationSearch from '../ConversationSearch';
+import {
+  Link,
+  useHistory,
+} from 'react-router-dom';
+import {
+  Jumbotron,
+  Spinner,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from 'reactstrap';
+import Moment from 'moment';
+import firebase from '../../Firebase.js';
 import ConversationListItem from './ListConversationItem.jsx';
 // import Toolbar from '../Toolbar';
 // import ToolbarButton from '../ToolbarButton';
+// import ConversationSearch from '../ConversationSearch';
 
 import './Message.css';
 
 axios.defaults.withCredentials = false;
 
 export default function ConversationList() {
-  const [conversations, setConversations] = useState({ list: [] });
+  // const [conversations, setConversations] = useState({ list: [] });
+  const [name, setName] = useState('');
+  const [conversations, setConversations] = useState([]);
+  const [showLoading, setShowLoading] = useState(true);
+  const [email, setEmail] = useState('');
+  const history = useHistory();
 
-  // const getConversations = () => {
-  //   axios.get('https://randomuser.me/api/?results=20').then((response) => {
-  //     console.log('axios ran');
-  //     const newConversations = response.data.results.map((result) => ({
-  //       // photo: result.picture.large,
-  //       name: `${result.name.first} ${result.name.last}`,
-  //       text: 'Hello world! This is a long message that needs to be truncated.',
-  //     }));
-  //     setConversations({ ...conversations, list: newConversations });
-  //     // console.log(newConversations);
-  //     console.log(conversations.list);
-  //     console.log('axios ran');
-  //   });
-  // };
-  // useEffect(() => {
-  //   getConversations();
-  // }, []);
+  const snapshotToArray = (snapshot) => {
+    const returnArr = [];
 
+    snapshot.forEach((childSnapshot) => {
+      const item = childSnapshot.val();
+      item.key = childSnapshot.key;
+      returnArr.push(item);
+    });
+    console.log(returnArr);
+
+    return returnArr;
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      setEmail(localStorage.getItem('email'));
+      firebase.database().ref('rooms/').on('value', (resp) => {
+        // console.log(resp);
+        // console.log(resp.val(), 'val');
+        setConversations([]);
+        setConversations(snapshotToArray(resp));
+        console.log(conversations);
+        setShowLoading(false);
+      });
+    };
+
+    fetchData();
+  }, []);
+  // send data that contain enter conversations status to Firebase DB
+  const enterChatRoom = (roomname) => {
+    // Welcome/Entry message for chat conversations. e.g "So and so joined the conversations"
+    const chat = {
+      roomname: '', email: '', message: '', date: '', type: '',
+    };
+    chat.roomname = roomname;
+    chat.email = email;
+    chat.date = Moment(new Date()).format('DD/MM/YYYY HH:mm:ss');
+    chat.message = `${email} enter the conversations `;
+    chat.type = 'join';
+    const newMessage = firebase.database().ref('chats/').push();
+    newMessage.set(chat);
+    // Retrieve roomusers table and query DB to find if user exist
+    firebase.database().ref('roomusers/').orderByChild('roomname').equalTo(roomname)
+      .on('value', (resp) => {
+        let roomuser = [];
+        roomuser = snapshotToArray(resp);
+        const user = roomuser.find((x) => x.email === email);
+        if (user !== undefined) {
+          const userRef = firebase.database().ref(`roomusers/${user.key}`);
+          userRef.update({ status: 'online' });
+        } else {
+          const newroomuser = { roomname: '', email: '', status: '' };
+          newroomuser.roomname = roomname;
+          newroomuser.email = email;
+          newroomuser.status = 'online';
+          const newRoomUser = firebase.database().ref('roomusers/').push();
+          newRoomUser.set(newroomuser);
+        }
+      });
+
+    history.push(`/chatroom/${roomname}`);
+  };
+
+  const convoListJsx = () => {
+    const convoList = conversations.map((convo, i) => (
+      <div key={convo.roomname}>
+        <ConversationListItem
+          title={convo.roomname}
+        />
+      </div>
+    ));
+    return convoList;
+  };
   return (
-    <div className="conversation-list">
-      {/* <Toolbar
+    <div>
+      {showLoading
+            && <Spinner color="primary" />}
+      <div className="conversations-list">
+        {convoListJsx()}
+        {}
+        {/* <Toolbar
         title="Messenger"
         leftItems={[
           <ToolbarButton key="cog" icon="ion-ios-cog" />,
@@ -41,37 +118,18 @@ export default function ConversationList() {
           <ToolbarButton key="add" icon="ion-ios-add-circle-outline" />,
         ]}
       /> */}
-      {/* <ConversationSearch /> */}
-      {/* {
-          conversations.list.map((conversation) => (
-            <ConversationListItem
-              key={conversation.name}
-              name={conversation.name}
-              text={conversation.text}
-              // photo={conversation.photo}
-            />
-          ))
-        } */}
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
-      <ConversationListItem />
+        {/* <ConversationSearch /> */}
+        {
+        // conversations.list.map((conversations) => (
+          //   <ConversationListItem
+          //     key={conversations.name}
+          //     name={conversations.name}
+          //     text={conversations.text}
+          //     // photo={conversations.photo}
+          //   />
+          // ))
+        }
+      </div>
     </div>
   );
 }
--- src/components/Messages/ListConversationItem.jsx
+++ src/components/Messages/ListConversationItem.jsx
@@ -3,15 +3,16 @@ import shave from 'shave';
 
 import './Message.css';
 
-export default function ConversationListItem() {
+export default function ConversationListItem({ title }) {
   useEffect(() => {
     shave('.conversation-snippet', 30);
   });
+  // const { photo, name, text } = data;
   return (
     <div className="conversation-list-item">
       <img className="conversation-photo" src="../photo.jpg" alt="conversation" />
       <div className="conversation-info">
-        <h1 className="conversation-title">Hello</h1>
+        <h1 className="conversation-title">{title}</h1>
         <p className="conversation-snippet">This is a long message. I think you will want to read this message as soon as possible. This is just a test for react, which is cool. i know i know.</p>
       </div>
     </div>
